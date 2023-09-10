32fcba5798332aa7579ca7102814fa8cf38695e1
Kamil <kamil.micota@goodsoft.pl>
2023-09-10 19:10:15 +0100

    Firebase set up before installing podfiles

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:email_sms_auth_firebase_example/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
